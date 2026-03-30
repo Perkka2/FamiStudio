9c63cc0b4aab0f33e4c0384a7560bab65d97cbf4
Steo <acidreflux64@protonmail.com>
2026-03-30 04:17:02 +0100

    Cleanup; use consts

--- FamiStudio/Source/Utils/TempoUtils.cs
+++ FamiStudio/Source/Utils/TempoUtils.cs
@@ -132,7 +132,7 @@ namespace FamiStudio
             }
             while ((grooveLength % notesPerBeat) != 0);
 
-            float numer = pal ? 3000.41892f : 3605.92884f; // (50.006982 : 60.098814) * 60
+            float numer = (pal ? NesApu.FpsPAL : NesApu.FpsNTSC) * 60;
             float denom = grooveNumFrames / (float)grooveLength * notesPerBeat;
 
             return numer / denom;
